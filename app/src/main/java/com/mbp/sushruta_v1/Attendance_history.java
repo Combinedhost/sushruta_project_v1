02be316f699dc3f755000d5c9c1a848ee441c5ce
Gowtham S <43443265+Gowtham135@users.noreply.github.com>
2020-03-29 11:43:15 +0530

    New layouts for location and attendance.

---
+++ app/src/main/java/com/mbp/sushruta_v1/Attendance_history.java
@@ -0,0 +1,13 @@
+package com.mbp.sushruta_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Attendance_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_history);
+    }
+}
