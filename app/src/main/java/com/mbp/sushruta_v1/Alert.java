a2836fcc9e6ccf9a2c7a869ee4435898b0dadc51
Mohan Kandhaiya <mohankandhaiya9@gmail.com>
2020-03-29 12:43:01 +0530

    gradle updated

--- app/src/main/java/com/mbp/sushruta_v1/Alert.java
+++ app/src/main/java/com/mbp/sushruta_v1/Alert.java
@@ -3,11 +3,27 @@ package com.mbp.sushruta_v1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
+
 public class Alert extends AppCompatActivity {
+
+    public static final String MESSAGE_STATUS = "message_status";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
+
+        WorkManager mWorkManager = WorkManager.getInstance();
+
+        PeriodicWorkRequest.Builder dayWorkBuilder =
+                new PeriodicWorkRequest.Builder(NotificationActivity.class, 1, TimeUnit.MINUTES, 5, TimeUnit.MINUTES);
+
+        PeriodicWorkRequest dayWork = dayWorkBuilder.build();
+
+        mWorkManager.enqueue(dayWork);
     }
 }
