1e0b70d45ac392e24ba42a238a77aba66626db0a
Gowtham S <43443265+Gowtham135@users.noreply.github.com>
2020-04-05 09:27:46 +0530

    Added logic to for patient activity

--- app/src/main/java/com/mbp/sushruta_v1/PatientLoginActivity.java
+++ app/src/main/java/com/mbp/sushruta_v1/PatientLoginActivity.java
@@ -1,16 +1,42 @@
 package com.mbp.sushruta_v1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class PatientLoginActivity extends AppCompatActivity {
+
+    private String verificationId;
+
+    //The edittext to input the code
+    private EditText otpCode, phoneNo;
+    private Button sendOtp, verifyOtp;
+    SharedPreferences sharedPref;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +45,132 @@ public class PatientLoginActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        otpCode = (EditText) findViewById(R.id.otp);
+        phoneNo = (EditText) findViewById(R.id.phone_no);
+
+        sendOtp = (Button) findViewById(R.id.send_sms);
+        verifyOtp = (Button) findViewById(R.id.verify_otp);
+
+        sharedPref = this.getSharedPreferences("mypref", Context.MODE_PRIVATE);
+
+        //Shared Preferences
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("patient_id", "GF4567");
+        editor.apply();
+
+        Intent intent = new Intent(getApplicationContext(), Patient_Information.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra("Patient", "GF4567");
+        startActivity(intent);
+
+
+        auth = FirebaseAuth.getInstance();
+
+        sendOtp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                sendVerificationCode(phoneNo.getText().toString());
             }
         });
+
+        verifyOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyVerificationCode(otpCode.getText().toString());
+            }
+        });
+
+    }
+
+
+    //the method is sending verification code
+    //the country id is concatenated
+    //you can take the country id as user input as well
+    private void sendVerificationCode(String mobile) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + mobile,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallbacks);
+    Log.i("Phone no ", mobile);
+    }
+
+
+    //the callback to detect the verification status
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            //Getting the code sent by SMS
+            String code = phoneAuthCredential.getSmsCode();
+
+            //sometime the code is not detected automatically
+            //in this case the code will be null
+            //so user has to manually enter the code
+            if (code != null) {
+                Log.i("PatientLoginActivity", code);
+//                editTextCode.setText(code);
+                //verifying the code
+//                verifyVerificationCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            Log.i("PatientLoginActivity", "code sent -  " + s);
+            //storing the verification id that is sent to the user
+            verificationId = s;
+        }
+    };
+
+
+    private void verifyVerificationCode(String code) {
+        //creating the credential
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+
+        //signing the user
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(PatientLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(PatientLoginActivity.this,"Successfull", Toast.LENGTH_LONG ).show();
+                            //verification successful we will start the profile activity
+//                            Intent intent = new Intent(getApplicationContext(), Patient_Information.class);
+//                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                            intent.putExtra("Patient", "Id");
+//                            startActivity(intent);
+
+                        } else {
+                            //verification unsuccessful.. display an error message
+                            String message = "Somthing is wrong, we will fix it soon...";
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                message = "Invalid code entered...";
+                            }
+
+                            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), message, Snackbar.LENGTH_LONG);
+                            snackbar.setAction("Dismiss", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                }
+                            });
+                            snackbar.show();
+                        }
+                    }
+                });
     }
 
 }
