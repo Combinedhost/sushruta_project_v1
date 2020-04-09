55af54f24ba6b5e1ba37b757a54815068227057a
Mohan Kandhaiya <mohankandhaiya9@gmail.com>
2020-04-09 22:59:50 +0530

    Update LanguageSwitchingActivity.java

--- app/src/main/java/com/mbp/sushruta_v1/LanguageSwitchingActivity.java
+++ app/src/main/java/com/mbp/sushruta_v1/LanguageSwitchingActivity.java
@@ -2,10 +2,14 @@ package com.mbp.sushruta_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class LanguageSwitchingActivity extends AppCompatActivity {
 
@@ -14,13 +18,33 @@ public class LanguageSwitchingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_switching);
 
+        loadLocale();
+
         Button tamil = (Button)findViewById(R.id.tamil);
 
         tamil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setLocale("ta_IN");
                 Toast.makeText(getApplicationContext(), "Language Switched", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("Language", MODE_PRIVATE).edit();
+        editor.putString("language", lang);
+        editor.apply();
+    }
+
+    public void loadLocale() {
+        SharedPreferences pref = getSharedPreferences("Language", MODE_PRIVATE);
+        String language = pref.getString("language", "");
+        setLocale(language);
     }
 }
